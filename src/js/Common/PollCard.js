a7924e6f0f6c2570f85421f318c7e2ce9a1cc177
Alex Young <Alex Young>
2017-01-05 00:02:27 +0000

    delete confirmation

--- src/js/Common/PollCard.js
+++ src/js/Common/PollCard.js
@@ -1,39 +1,65 @@
 import React from 'react';
 import connect from '../Redux/connect';
-import {deletePoll} from '../Redux/ActionCreators/PollActions';
+import { deletePoll, showDeletePollConfirm, cancelDelete } from '../Redux/ActionCreators/PollActions';
 import { LinkContainer } from 'react-router-bootstrap';
 import { ButtonToolbar, Button, Panel } from 'react-bootstrap';
+import Fa from 'react-fontawesome';
 
-const PollCard = props => {
-    const deleteButton = (
-        <Button bsStyle="danger" onClick={() => props.deletePoll(props.poll._id)}>
-            Delete poll
-        </Button>
-    );
+class PollCard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            confirmDelete: false
+        }
+    }
 
-    return (
-        <Panel>
-            <h3>{props.poll.question}</h3>
-            <h5>{props.poll.answers.length} options to choose from!</h5>
-            <ButtonToolbar>
-                <LinkContainer to={"/polls/view/" + props.poll._id}>
-                    <Button bsStyle="primary">
-                        Vote Now
-                    </Button>
-                </LinkContainer>
-                <LinkContainer to={"/polls/results/" + props.poll._id}>
-                    <Button bsStyle="primary">
-                        View Results
-                    </Button>
-                </LinkContainer>
-                {props.showDelete && deleteButton}
-            </ButtonToolbar>
-        </Panel>
-    );    
+    toggleDeleteConfirm(show) {
+        this.setState({
+            confirmDelete: show
+        })
+    }
+
+    render() {
+        const deleteButton = (
+            <Button bsStyle="danger" onClick={() => this.toggleDeleteConfirm(true)}>
+                <Fa name="trash-o" />
+                Delete poll
+            </Button>
+        );
+
+        const deleteCancel = (
+            <Button bsStyle="info" onClick={() => this.toggleDeleteConfirm(false)}>
+                Not Really
+            </Button>
+        );
+        const deleteConfirm = (
+            <Button bsStyle="danger" onClick={() => this.props.deletePoll(this.props.poll._id)}>
+                Seriously Delete!
+            </Button>
+        );
+
+        return (
+            <Panel>
+                <h3>{this.props.poll.question}</h3>
+                <h5>{this.props.poll.answers.length} options to choose from!</h5>
+                <ButtonToolbar>
+                    <LinkContainer to={"/polls/view/" + this.props.poll._id}>
+                        <Button bsStyle="primary">
+                            View Results
+                        </Button>
+                    </LinkContainer>
+                    {this.props.showDelete && !this.state.confirmDelete && deleteButton}                      
+                    {this.props.showDelete && this.state.confirmDelete && deleteCancel}                  
+                    {this.props.showDelete && this.state.confirmDelete && deleteConfirm}
+                </ButtonToolbar>
+            </Panel>
+        );   
+    }
+     
 };
 
 export default connect(
-    {},
+    null,
     {
         deletePoll
     }
