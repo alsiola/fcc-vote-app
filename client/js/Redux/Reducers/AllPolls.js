a7d43a229d0dbfc7aa1b6ea7537af5325a14a94f
Alex Young <Alex Young>
2017-01-09 16:09:42 +0000

    formatting

--- client/js/Redux/Reducers/AllPolls.js
+++ client/js/Redux/Reducers/AllPolls.js
@@ -1,22 +1,28 @@
 import * as actions from '../Actions';
-import { handle } from 'redux-pack';
-import { Except, IndexWhere } from '../Utils/JsLinq';
+import {handle} from 'redux-pack';
+import {Except, IndexWhere} from '../Utils/JsLinq';
 
 export default function AllPolls(state = [], action) {
-    const { type, payload } = action;
+    const {type, payload} = action;
 
     switch (type) {
         case actions.NEW_POLL_SAVED:
             return handle(state, action, {
-                success: s => [...s, ...payload.data.poll]
+                success: s => [
+                    ...s,
+                    ...payload.data.poll
+                ]
             });
         case actions.POLL_LOADED:
             return handle(state, action, {
-                success: s => [...s, payload.data.poll]
+                success: s => [
+                    ...s,
+                    payload.data.poll
+                ]
             });
         case actions.POLLS_LOADED:
             return handle(state, action, {
-                success: s=> [...payload.data.polls]
+                success: s => [...payload.data.polls]
             });
         case actions.DELETE_POLL:
             return handle(state, action, {
@@ -26,7 +32,10 @@ export default function AllPolls(state = [], action) {
             return handle(state, action, {
                 success: s => {
                     if (payload.data.success) {
-                        return [...Except(s, [payload.data.poll], (a, b) => a._id === b._id), payload.data.poll].sort((a,b) => {
+                        return [
+                            ...Except(s, [payload.data.poll], (a, b) => a._id === b._id),
+                            payload.data.poll
+                        ].sort((a, b) => {
                             return new Date(b.creation_date) - new Date(a.creation_date);
                         });
                     }
--- client/js/Redux/Selectors/PollSelectors.js
+++ client/js/Redux/Selectors/PollSelectors.js
@@ -1,11 +1,16 @@
 export const userPolls = state => state.Polls;
 
 export const getPollById = (state, props) => {
-    return state.AllPolls.filter(poll => poll._id === props.params.pollId)[0];
+    return state
+        .AllPolls
+        .filter(poll => poll._id === props.params.pollId)[0];
 }
 
 export const getAllPollsDateDesc = (state, props) => {
-    return state.AllPolls.sort((a, b) => {
-        return a.creation_date - b.creation_date;
-    }).slice(0, 10);
+    return state
+        .AllPolls
+        .sort((a, b) => {
+            return a.creation_date - b.creation_date;
+        })
+        .slice(0, 10);
 }
