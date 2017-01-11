b19c894a64eba208c194c0d756cca67b80f49be7
Alex Young <Alex Young>
2017-01-11 14:59:14 +0000

    --twitter share

--- client/js/Common/ShareButtons.js
+++ client/js/Common/ShareButtons.js
@@ -3,13 +3,13 @@ import { ShareButtons } from 'react-share';
 import { Button } from 'react-bootstrap';
 import Fa from 'react-fontawesome';
 
-const { FacebookShareButton } = ShareButtons;
+const { TwitterShareButton } = ShareButtons;
 
 export default props => (
-    <FacebookShareButton url={props.url} title={props.title}>
+    <TwitterShareButton url={props.url} title={props.title}>
         <Button bsStyle="primary">
             <Fa name="facebook" size="2x" />
-            Share on Facebook
+            Share on Twitter
         </Button>
-    </FacebookShareButton>
+    </TwitterShareButton>
 );
